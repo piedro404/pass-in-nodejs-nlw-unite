6b741e45b7af8a7a1c29a77069857974c8192b3a
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-04-05 14:17:35 -0300

    feat: :seedling: DataBase API

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
 import fastify from "fastify";
+
+const app = fastify()
+
+app.get('/', () => {
+    return {"Hello NLW UNITE": "Oi"}
+})
+
+app.listen({port: 3333})
+.then(() => {
+    console.log("HTTP server RUN")
+})
