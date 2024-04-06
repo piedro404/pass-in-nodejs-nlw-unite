1f2f7d47319cb15197c5a42853eb75bdc3b5f3d4
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-04-06 20:02:11 -0300

    feat: :sparkles: Create Route Get Event Details

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import {
 } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
 
 const app = fastify();
 
@@ -14,6 +15,7 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent)
 app.register(registerForEvent)
+app.register(getEvent)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
