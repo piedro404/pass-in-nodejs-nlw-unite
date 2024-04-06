20177f9c56dfa2bf1c9c7e3c2d789fe5947fd2e1
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-04-06 14:27:16 -0300

    feat: :goal_net: Catch Erros in Application

--- src/routes/register-for-event.ts
+++ src/routes/register-for-event.ts
@@ -25,6 +25,37 @@ export async function registerForEvent(app: FastifyInstance) {
         const { eventId } = request.params
         const { name, email } = request.body
 
+        const attendeeFromEmail = await prisma.attendee.findUnique({
+            where: {
+                eventId_email: {
+                    email,
+                    eventId
+                }
+            }
+        })
+
+        if (attendeeFromEmail !== null) {
+            throw new Error("This e-mail is already register for this event.")
+        }
+
+        const [event, amountOfAttendeesForEvent] = await Promise.all([
+            prisma.event.findUnique({
+                where: {
+                    id: eventId,
+                }
+            }),
+
+            prisma.attendee.count({
+                where: {
+                    eventId,
+                }
+            })
+        ])
+
+        if (event?.maximumAttendees && amountOfAttendeesForEvent >= event?.maximumAttendees) {
+            throw new Error("The maximum number of attendees for this event has been reached.")
+        }
+
         const attendee = await prisma.attendee.create({
             data: {
                 name,
