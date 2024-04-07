b243c447b80c707d80b110f92c65efc18266d243
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-04-07 19:50:45 -0300

    feat: :seedling: Created Route Get Attendees by Event

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
+import { getEventAttendees } from "./routes/get-event-attendees";
 
 const app = fastify();
 
@@ -20,6 +21,7 @@ app.register(registerForEvent)
 app.register(getEvent)
 app.register(getAttendeeBadge)
 app.register(checkIn)
+app.register(getEventAttendees)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
