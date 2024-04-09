11abc56ca832e8d6db84abd6b7d1f62338560a3f
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-04-09 17:38:45 -0300

    feat: :bug: Add z.object in response routes

--- src/routes/get-event.ts
+++ src/routes/get-event.ts
@@ -4,60 +4,62 @@ import { z } from "zod";
 import { prisma } from "../lib/prisma";
 
 export async function getEvent(app: FastifyInstance) {
-  app
-    .withTypeProvider<ZodTypeProvider>()
-    .get("/events/:eventId", {
-        schema: {
-            params: z.object({
-                eventId: z.string().uuid()
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/events/:eventId",
+    {
+      schema: {
+        params: z.object({
+          eventId: z.string().uuid(),
+        }),
+        response: {
+          200: z.object({
+            event: z.object({
+              id: z.string().uuid(),
+              title: z.string(),
+              slug: z.string(),
+              details: z.string().nullable(),
+              maximumAttendees: z.number().int().nullable(),
+              attendeesAmount: z.number().int(),
             }),
-            response: {
-                200: {
-                    event: z.object({
-                        id: z.string().uuid(),
-                        title: z.string(),
-                        slug: z.string(),
-                        details: z.string().nullable(),
-                        maximumAttendees: z.number().int().nullable(),
-                        attendeesAmount: z.number().int(),
-                    })
-                },
-            },
-        }
-    }, async (request, reply) => {
-        const { eventId } = request.params
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { eventId } = request.params;
 
-        const event = await prisma.event.findUnique({
+      const event = await prisma.event.findUnique({
+        select: {
+          id: true,
+          title: true,
+          slug: true,
+          details: true,
+          maximumAttendees: true,
+          _count: {
             select: {
-                id: true,
-                title: true,
-                slug: true,
-                details: true,
-                maximumAttendees: true,
-                _count: {
-                    select: {
-                        attendees: true,
-                    }
-                },
+              attendees: true,
             },
-            where: {
-                id: eventId,
-            }
-        })
+          },
+        },
+        where: {
+          id: eventId,
+        },
+      });
 
-        if (event === null) {
-            throw new Error("Event not found.")
-        }
+      if (event === null) {
+        throw new Error("Event not found.");
+      }
 
-        return reply.status(200).send({
-            event: {
-                id: event.id,
-                title: event.title,
-                slug: event.slug,
-                details: event.details,
-                maximumAttendees: event.maximumAttendees,
-                attendeesAmount: event._count.attendees,
-            },
-        })
-    });
+      return reply.status(200).send({
+        event: {
+          id: event.id,
+          title: event.title,
+          slug: event.slug,
+          details: event.details,
+          maximumAttendees: event.maximumAttendees,
+          attendeesAmount: event._count.attendees,
+        },
+      });
+    }
+  );
 }
