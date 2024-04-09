d78b3630f0c3c30ec3498e60b6cb44f43d0d6b20
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-04-09 17:37:23 -0300

    feat: :sparkles: Create Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import fastify from "fastify";
-import {
-  serializerCompiler,
-  validatorCompiler
-} from "fastify-type-provider-zod";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
@@ -12,9 +12,26 @@ import { getEventAttendees } from "./routes/get-event-attendees";
 
 const app = fastify();
 
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    info: {
+      title: 'pass.in',
+      description: 'Especificações da API para o back-end da aplicação pass.in construída durante o NLW Unite da Rocketseat.',
+      version: '1.0.0'
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
 // Add schema validator and serializer
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs'
+})
 
 app.register(createEvent)
 app.register(registerForEvent)
