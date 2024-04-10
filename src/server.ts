83475d30081a79719a3cdcef91766de92d62e07f
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-04-09 21:09:45 -0300

    feat: :seedling: Add Cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import fastify from "fastify";
+
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
+import fastifyCors from "@fastify/cors";
 
 import { serializerCompiler, validatorCompiler, jsonSchemaTransform } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
@@ -14,6 +16,10 @@ import { getEventAttendees } from "./routes/get-event-attendees";
 import { errorHandler } from "./error-handler";
 
 const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+})
 
 app.register(fastifySwagger, {
   swagger: {
@@ -45,6 +51,6 @@ app.register(getEventAttendees)
 
 app.setErrorHandler(errorHandler)
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("HTTP server running!");
 });
